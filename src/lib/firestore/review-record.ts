66a8efb3c4030a8ee98ff4ce9a46afa550e5324f
PabloRius <pablogrius@gmail.com>
2025-09-11 18:35:39 +0200

    Add timestamp to review-record creation

--- src/lib/firestore/review-record.ts
+++ src/lib/firestore/review-record.ts
@@ -1,4 +1,4 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../firebase/clientApp";
 
 export async function updateRecord(
@@ -6,6 +6,8 @@ export async function updateRecord(
   userId: string,
   groupId: string
 ) {
+  const recordRef = doc(db, "review-record", name);
+
   const listRef = doc(
     db,
     "review-record",
@@ -13,5 +15,15 @@ export async function updateRecord(
     "lists",
     `${userId}_${groupId}`
   );
+
+  const recordSnap = await getDoc(recordRef);
+
+  if (!recordSnap.exists()) {
+    await setDoc(recordRef, {
+      createdAt: new Date(),
+      status: "alive",
+    });
+  }
+
   await setDoc(listRef, { userId, groupId });
 }
